760d9460560ed702fcdb668851eb0eb3d43a80aa
Sergey Grigorash <adviser@bk.ru>
2024-06-25 12:29:51 +0500

    feat: Add booksError action creator for handling errors in BookList component

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -4,19 +4,24 @@ import BookListItem from "../book-list-item";
 import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
 import { withBookstoreService } from "../hoc";
-import { booksLoaded, booksRequested } from "../../actions";
+import { booksLoaded, booksRequested, booksError } from "../../actions";
 import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 class BookList extends Component {
   componentDidMount() {
     // получение данных из сервиса
-    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested, booksError } =
+      this.props;
     booksRequested(); // вызов action creator без аргументов и передача action в redux store через mapDispatchToProps (в данном случае через booksRequested) для обновления state в redux store (в данном случае обновление loading в state.loading) и перерисовки компонента (в данном случае перерисовка спиннера) с новыми данными (в данном случае с данными из сервиса)
-    bookstoreService.getBooks().then((data) => booksLoaded(data)); // вызов action creator с данными из сервиса (полученными в результате работы промиса) в качестве аргумента (в данном случае массив книг) и передача action в redux store через mapDispatchToProps (в данном случае через booksLoaded) для обновления state в redux store (в данном случае обновление массива книг в state.books) и перерисовки компонента (в данном случае перерисовка списка книг) с новыми данными (в данном случае с данными из сервиса)
+    bookstoreService
+      .getBooks()
+      .then((data) => booksLoaded(data))
+      .catch((err) => booksError(err)); // вызов action creator с данными из сервиса (полученными в результате работы промиса) в качестве аргумента (в данном случае массив книг) и передача action в redux store через mapDispatchToProps (в данном случае через booksLoaded) для обновления state в redux store (в данном случае обновление массива книг в state.books) и перерисовки компонента (в данном случае перерисовка списка книг) с новыми данными (в данном случае с данными из сервиса)
   }
 
   render() {
-    const { books, loading } = this.props;
+    const { books, loading, error } = this.props;
     if (loading) {
       return (
         <div>
@@ -24,6 +29,11 @@ class BookList extends Component {
         </div>
       );
     }
+
+    if (error) {
+      return <ErrorIndicator />;
+    }
+
     return (
       <ul className="book-list">
         {books.map((book) => {
@@ -42,6 +52,7 @@ const mapStateToProps = (state) => {
   return {
     books: state.books,
     loading: state.loading,
+    error: state.error,
   };
 };
 
@@ -72,7 +83,7 @@ const mapStateToProps = (state) => {
 // };
 
 // ВАРИАНТ 4
-const mapDispatchToProps = { booksLoaded, booksRequested }; // здесь мы используем объект вместо функции, чтобы автоматически обернуть action creator в dispatch и возвращаем объект с action creator в качестве свойств (в данном случае один action creator) и redux сам все сделает за нас (примерно как вариант 3)
+const mapDispatchToProps = { booksLoaded, booksRequested, booksError }; // здесь мы используем объект вместо функции, чтобы автоматически обернуть action creator в dispatch и возвращаем объект с action creator в качестве свойств (в данном случае один action creator) и redux сам все сделает за нас (примерно как вариант 3)
 
 export default withBookstoreService()(
   connect(mapStateToProps, mapDispatchToProps)(BookList)
