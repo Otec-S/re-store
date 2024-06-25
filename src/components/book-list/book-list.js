d5b3ec521e575d1e7971f3770e1b61d54cffbfd4
Sergey Grigorash <adviser@bk.ru>
2024-06-25 11:56:50 +0500

    chore: Add booksRequested action creator for loading state in BookList component

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -4,13 +4,14 @@ import BookListItem from "../book-list-item";
 import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
 import { withBookstoreService } from "../hoc";
-import { booksLoaded } from "../../actions";
+import { booksLoaded, booksRequested } from "../../actions";
 import Spinner from "../spinner";
 
 class BookList extends Component {
   componentDidMount() {
     // получение данных из сервиса
-    const { bookstoreService, booksLoaded } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+    booksRequested(); // вызов action creator без аргументов и передача action в redux store через mapDispatchToProps (в данном случае через booksRequested) для обновления state в redux store (в данном случае обновление loading в state.loading) и перерисовки компонента (в данном случае перерисовка спиннера) с новыми данными (в данном случае с данными из сервиса)
     bookstoreService.getBooks().then((data) => booksLoaded(data)); // вызов action creator с данными из сервиса (полученными в результате работы промиса) в качестве аргумента (в данном случае массив книг) и передача action в redux store через mapDispatchToProps (в данном случае через booksLoaded) для обновления state в redux store (в данном случае обновление массива книг в state.books) и перерисовки компонента (в данном случае перерисовка списка книг) с новыми данными (в данном случае с данными из сервиса)
   }
 
@@ -71,7 +72,7 @@ const mapStateToProps = (state) => {
 // };
 
 // ВАРИАНТ 4
-const mapDispatchToProps = { booksLoaded }; // здесь мы используем объект вместо функции, чтобы автоматически обернуть action creator в dispatch и возвращаем объект с action creator в качестве свойств (в данном случае один action creator) и redux сам все сделает за нас (примерно как вариант 3)
+const mapDispatchToProps = { booksLoaded, booksRequested }; // здесь мы используем объект вместо функции, чтобы автоматически обернуть action creator в dispatch и возвращаем объект с action creator в качестве свойств (в данном случае один action creator) и redux сам все сделает за нас (примерно как вариант 3)
 
 export default withBookstoreService()(
   connect(mapStateToProps, mapDispatchToProps)(BookList)
